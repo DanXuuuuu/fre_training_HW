2e892b4cd118db5e63ad01b05e8ac578384abbf8
DanXuuuuu <139434572+DanXuuuuu@users.noreply.github.com>
2025-01-10 21:03:06 -0800

    update movie-list and movie-item

--- Notflix/src/app/app.module.ts
+++ Notflix/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { MovieItemComponent } from './components/movie-item/movie-item.component';
 import { MovieListComponent } from './components/movie-list/movie-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { TruncatePipe } from './truncate.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     MovieItemComponent,
     MovieListComponent,
+    TruncatePipe,
 
   ],
   imports: [
